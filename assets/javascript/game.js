7c4ad4ce9dc8a53734f3f87c2247d6647c4cb08d
Ronald Glover <ron881212@gmail.com>
2018-11-22 05:59:53 -0500

    crreated ids and objects for players

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+// Character Objects
+
+var playerRey = {
+    name: "Rey",
+    gender: "Female",
+    affiliation: "Resistance",
+    homeworld: "Jakku",
+    hp: 150
+};
+
+var playerFinn = {
+    name: "Finn",
+    gender: "Male",
+    affiliation: "Resistance",
+    homeworld: "Artorias",
+    hp: 200
+};
+
+var playerPhasma = {
+    name: "Captain Phasma",
+    gender: "Female",
+    affiliation: "First Order",
+    homeworld: "Parnassos",
+    hp: 300
+};
+
+var playerSnoke = {
+    name: "Supreme Leader Snoke",
+    gender: "Male",
+    affiliation: "First Order",
+    homeworld: "Unknown",
+    hp: 400
+};
