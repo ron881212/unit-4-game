df58cc4c2aa89c8fa46456bcd3cc38611145003d
ron881212 <ron881212@gmail.com>
2018-11-22 19:06:26 -0500

    made of the players and logos interactive

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,7 +17,6 @@ var playerFinn = {
     affiliation: "Resistance",
     homeworld: "Artorias",
     hp: 200,
-    hp: 150,
     atck1: function(){
         var thisNotYetDefind;
     }
@@ -29,7 +28,6 @@ var playerPhasma = {
     affiliation: "First Order",
     homeworld: "Parnassos",
     hp: 300,
-    hp: 150,
     atck1: function(){
         var thisNotYetDefind;
     }
@@ -41,7 +39,6 @@ var playerSnoke = {
     affiliation: "First Order",
     homeworld: "Unknown",
     hp: 400,
-    hp: 150,
     atck1: function(){
         var thisNotYetDefind;
     }
@@ -103,14 +100,38 @@ $("#snokeLogo").mouseover(function(){
     });
 });
 
+// Displays Rey info when you click on her
 
 $("#reyPlayer").on("click", function(){
-    $("#fightCard").append('<img class="card-img-top" src="assets/images/rey.png" alt="logo img">');
-    $("#fightCard").append('<div class="card-body">');
-    $("#fightCard").append('<h3 class="card-text">Rey</h3>');
-    $("#fightCard").append('<h4>Gender: Female</h4>');
-    $("#fightCard").append('<h4>Affiliation: Resistance<h4>');
-    $("#fightCard").append('<h4> Homeworld: Jakku</h4>');
-    $("#fightCard").append('<h4>HP: 150</h4>');
-    break;
+    $("#fightCard").html('<img class="card-img-top" src="assets/images/rey.png" alt="logo img">' + '<div class="card-body">' + '<h3 class="card-text">Rey</h3>' + '<h4>Gender: Female</h4>' + '<h4>Affiliation: Resistance<h4>' + '<h4> Homeworld: Jakku</h4>' + '<h4>HP: 150</h4>');
+});
+
+// Displays Finn info when you click on him
+
+$("#finnPlayer").on("click", function(){
+    $("#fightCard").html('<img class="card-img-top" src="assets/images/finn.jpg" alt="logo img">' + '<div class="card-body">' + '<h3 class="card-text">Finn</h3>' + '<h4>Gender: Male</h4>' + '<h4>Affiliation: Resistance<h4>' + '<h4> Homeworld: Artorias</h4>' + '<h4>HP: 200</h4>');
+});
+
+// Displays Phasma info when you click on her
+
+$("#capPlayer").on("click", function(){
+    $("#fightCard").html('<img class="card-img-top" src="assets/images/captain_phasma.jpg" alt="logo img">' + '<div class="card-body">' + '<h3 class="card-text">Captain Phasma</h3>' + '<h4>Gender: Female</h4>' + '<h4>Affiliation: First Order<h4>' + '<h4> Homeworld: Parnassos</h4>' + '<h4>HP: 300</h4>');
+});
+
+// Displays Surpreme Leader Snoke info when you click on him
+
+$("#snokePlayer").on("click", function(){
+    $("#fightCard").html('<img class="card-img-top" src="assets/images/snoke.jpg" alt="logo img">' + '<div class="card-body">' + '<h3 class="card-text">Surpreme Leader Snoke</h3>' + '<h4>Gender: Male</h4>' + '<h4>Affiliation: First Order<h4>' + '<h4> Homeworld: Unknown</h4>' + '<h4>HP: 400</h4>');
+});
+
+//Displays The Resistances info when you click on the logo
+
+$("#reyLogo, #finnLogo").on("click", function(){
+    $("#fightCard").html('<img class="card-img-top" src="assets/images/logo1.jpg" alt="logo img">' + '<div class="card-body">' + '<h3 class="card-text">The Resistance</h3>' + '<h4> Resistance movement is a private paramilitary force led by General Leia Organa that opposes the First Order<h4>');
+});
+
+//Displays First Order info when you click on the logo
+
+$("#capLogo, #snokeLogo").on("click", function(){
+    $("#fightCard").html('<img class="card-img-top" src="assets/images/logo2.jpg" alt="logo img">' + '<div class="card-body">' + '<h3 class="card-text">First Order</h3>' + '<h4>The First Order is an autocratic military dictatorship. It is the canonical counterpart of the Dark Empire.<h4>');
 });
